dedc05837852d41d9d9dc73438755a3cac11705c
Disco18 <tlrthurley@gmail.com>
2024-11-02 23:44:58 +1100

    CSV importation
    Added the function to import csv files and read the data, the extremely early adaptation of importing manifests and using the data to generate load plans.

--- backend.py
+++ backend.py
@@ -1,16 +1,27 @@
 #the backend will be where all the logic happens to make Lacky work
 import sys
 import tkinter
+import pandas as pd
+
+def importMainfest(filename, sheet_name="Sheet1"):
+    #This function will import the data from the manifest via spreadsheet.
+    try:
+        data = pd.read_excel(filename, sheet_name=sheet_name)
+        print("Data loaded successfully.")
+        return data
+    
+    except FileNotFoundError:
+        print("THe file was not found.")
+
+exc = importMainfest("Testsheet.xlsx", "Sheet1")
+print(exc.head())
 
 
-def createLoadPlan():
-    #Takes the manifest and blends it with the paramaiters to create the ideal load plan.
 
+#def trailerDimenions():
+#This function will define the layout and measurements of the trailers in use.
+#The name of this function may change.
+#Need to create a function for selecting the correct trailers the user wants to use.
 
-def importMainfest():
-    #This function will import the data from the manifest via spreadsheet. 
-
-def trailerDimenions():
-    #This function will define the layout and measurements of the trailers in use.
-    #The name of this function may change.
-    #Need to create a function for selecting the correct trailers the user wants to use.
+#def createLoadPlan():
+#Takes the manifest and blends it with the paramaiters to create the ideal load plan.
